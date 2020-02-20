52e83c4147ef38c5fbe3e912914e686724755a3e
ZHF <mynamezhf@gmail.com>
2020-02-20 23:43:02 +0800

    更新了评分功能

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	header("Content-type: text/html; charset=utf-8");
 	$MySqlHost = "localhost";
 	$MySqlUser = "root";
-	$MySqlPwd = "S6$@JzXkll1zK@NC";
-	$MySqlDatabaseName = "zhf_cloud";
+	$MySqlPwd = "";
+	$MySqlDatabaseName = "all_img_praise";
 	
 ?>
