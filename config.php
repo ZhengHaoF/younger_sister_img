108c4f48d9891bb5ec388e870f5fc3b26c086669
ZHF <mynamezhf@gmail.com>
2020-02-20 23:38:28 +0800

    更新了评分功能

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+	header("Content-type: text/html; charset=utf-8");
+	$MySqlHost = "localhost";
+	$MySqlUser = "root";
+	$MySqlPwd = "S6$@JzXkll1zK@NC";
+	$MySqlDatabaseName = "zhf_cloud";
+	
+?>
