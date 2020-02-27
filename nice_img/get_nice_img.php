12d3de913ab85ecc5768efce8fb0372efd5cf114
ZHF <mynamezhf@gmail.com>
2020-02-27 17:59:30 +0800

    1.1.5更新了精品区点赞人数

--- nice_img/get_nice_img.php
+++ nice_img/get_nice_img.php
@@ -5,9 +5,12 @@ mysqli_query($conn, "set character set 'utf8'");//读库
 mysqli_query($conn,"set names 'utf8'");//写库
 $res = mysqli_query($conn,"SELECT * FROM img_praise WHERE praise > tread");
 $x = array();
+$y = array();
 for ($i=0;$i<=mysqli_num_rows($res) - 1;$i++){
     $item = mysqli_fetch_row($res);
     //echo "https://web-1253780623.cos.ap-shanghai.myqcloud.com/younger_sister_img/" . $item[0]. ".jpg" . "<br/>";
     $x[$i] = "https://web-1253780623.cos.ap-shanghai.myqcloud.com/younger_sister_img/" . $item[0]. ".jpg";
+    $y[$i] = $item[1];
 }
-echo json_encode($x);
+
+echo "{" . "\"url\":" . json_encode($x) . ",\"praise\":" . json_encode($y) . "}";
