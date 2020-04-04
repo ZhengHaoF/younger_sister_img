bf60263eff92fa73c33c9bb43030e250edf91804
ZHF <mynamezhf@gmail.com>
2020-04-04 23:40:52 +0800

    新增www.nyato.com的爬虫和640余张图片

--- Python程序/www.nyato.com爬虫/main.py
+++ Python程序/www.nyato.com爬虫/main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 img_group = 25717 #初始图集编号
 for num in range(1, 6):
-    page_url = "https://www.nyato.com/index.php?app=public&mod=Topic&act=index&k=%E7%BB%9D%E5%AF%B9%E9%A2%86%E5%9F%9F&p=" + str(1)
+    page_url = "https://www.nyato.com/index.php?app=public&mod=Topic&act=index&k=%E7%BB%9D%E5%AF%B9%E9%A2%86%E5%9F%9F&p=" + str(num)
     response = requests.get(page_url)
     response.encoding = 'utf-8'
     html = response.text
---
+++ Python程序/www.nyato.com爬虫/test.py
@@ -0,0 +1,3 @@
+for num in range(1, 6):
+    page_url = "https://www.nyato.com/index.php?app=public&mod=Topic&act=index&k=%E7%BB%9D%E5%AF%B9%E9%A2%86%E5%9F%9F&p=" + str(num)
+    print(page_url)
